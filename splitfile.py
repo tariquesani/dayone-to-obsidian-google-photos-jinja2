d5d8724c8d6092151cc8a6dd66037021be382ec7
Lucy <17546625+LucyDYu@users.noreply.github.com>
2023-07-05 17:40:53 +0800

    Modify splitfile

--- splitfile.py
+++ splitfile.py
@@ -1,116 +1,178 @@
-import datetime
-import dateutil.parser
-import json
-import pytz    # pip install pytz
-import re
-import os
+import logging
 import sys
+
+import dateutil.parser
+import pytz  # pip install pytz
+import os
 import shutil
 import time
+import json
+import re
+
+from processor.AudioEntryProcessor import AudioEntryProcessor
+from processor.EntryProcessor import EntryProcessor
+from processor.PdfEntryProcessor import PdfEntryProcessor
+from processor.PhotoEntryProcessor import PhotoEntryProcessor
+from processor.VideoEntryProcessor import VideoEntryProcessor
+from config.config import Config
+
+
+def rename_media(root, subpath, media_entry, filetype):
+    pfn = os.path.join(root, subpath, '%s.%s' % (media_entry['md5'], filetype))
+    if os.path.isfile(pfn):
+        newfn = os.path.join(root, subpath, '%s.%s' % (media_entry['identifier'], filetype))
+        print('Renaming %s to %s' % (pfn, newfn))
+        os.rename(pfn, newfn)
+
+
+# Load the configuration
+Config.load_config("config.yaml")
+DEFAULT_TEXT = Config.get("DEFAULT_TEXT", "")
+
+# Initialize the EntryProcessor class variables
+EntryProcessor.initialize()
 
 # Set this as the location where your Journal.json file is located
-root = r"D:\OneDrive\Documents\dayone" 
-icons = False   # Set to true if you are using the Icons Plugin in Obsidian
-tagPrefix = "#journal/"  # This will append journal/ as part of the tag name for sub-tags ie. instead of #entry, it is #journal/entry. To exclude set to "". If you change journal to something else, make sure you keep the trailing /
+root = Config.get("ROOT")
+icons = False  # Set to true if you are using the Icons Plugin in Obsidian
 
-
-journalFolder = os.path.join(root, "journal") #name of folder where journal entries will end up
-fn = os.path.join( root, "Journal.json" )
+# name of folder where journal entries will end up
+journalFolder = os.path.join(root, Config.get("JOURNAL_FOLDER"))
+fn = os.path.join(root, Config.get("JOURNAL_JSON"))
 
 # Clean out existing journal folder, otherwise each run creates new files
 if os.path.isdir(journalFolder):
-    print ("Deleting existing folder: %s" % journalFolder)
+    print("Deleting existing folder: %s" % journalFolder)
     shutil.rmtree(journalFolder)
+# Give time for folder deletion to complete. Only a problem if you have the folder open when trying to run the script
+time.sleep(2)
 
-time.sleep(2)  # Give time for folder deletion to complete. Only a problem if you have the folder open when trying to run the script
 if not os.path.isdir(journalFolder):
-    print( "Creating journal folder: %s" % journalFolder)
+    print("Creating journal folder: %s" % journalFolder)
     os.mkdir(journalFolder)
 
 if icons:
-    print ("Icons are on")
+    print("Icons are on")
     dateIcon = "`fas:CalendarAlt` "
 else:
-    print ("Icons are off")
-    dateIcon = ""  #make 2nd level heading
+    print("Icons are off")
+    dateIcon = ""  # make 2nd level heading
 
-
-print( "Begin processing entries")
+print("Begin processing entries")
 count = 0
 with open(fn, encoding='utf-8') as json_file:
     data = json.load(json_file)
+
+    photo_processor = PhotoEntryProcessor()
+    audio_processor = AudioEntryProcessor()
+    video_processor = VideoEntryProcessor()
+    pdf_processor = PdfEntryProcessor()
+
     for entry in data['entries']:
         newEntry = []
 
         createDate = dateutil.parser.isoparse(entry['creationDate'])
-        localDate = createDate.astimezone(pytz.timezone(entry['timeZone'])) # It's natural to use our local date/time as reference point, not UTC
+        localDate = createDate.astimezone(
+            pytz.timezone(entry['timeZone']))  # It's natural to use our local date/time as reference point, not UTC
 
-        # Add location
-        location = ''
-        for locale in ['placeName', 'localityName', 'administrativeArea', 'country']:
-            try:
-                location = "%s, %s" % (location, entry['location'][locale] )
-            except KeyError:
-                pass
-        location = location[2:]
+        # Format the date and time with the weekday
+        formatted_datetime = createDate.strftime("%Y-%m-%d %H:%M:%S %A")
 
-        dateCreated = str(createDate)
+        dateCreated = formatted_datetime
         coordinates = ''
- 
-        frontmatter = '''---
-- created: ''' + dateCreated + '''
-'''
-        
+
+        frontmatter = f"---\n" \
+                      f"date: {dateCreated}\n"
+
+        weather = EntryProcessor.get_weather(entry)
+        if len(weather) > 0:
+            frontmatter += f"weather: {weather}\n"
+        tags = EntryProcessor.get_tags(entry)
+        if len(tags) > 0:
+            frontmatter += f"tags: {tags}\n"
+
         if 'location' in entry:
-            coordinates = str(entry['location']['latitude']) + ',' + str(entry['location']['longitude'])
-            frontmatter = frontmatter +  '- location: [' + coordinates + ']'
-        frontmatter = frontmatter + '''
----
-'''
+            coordinates = EntryProcessor.get_coordinates(entry)
+
+        frontmatter += "locations: \n"
+        frontmatter += "---\n"
+
         newEntry.append(frontmatter)
- 
+
+        # If you want time as entry title, uncomment below.
         # Add date as page header, removing time if it's 12 midday as time obviously not read
-        if sys.platform == "win32":
-            newEntry.append( '## %s%s\n' % (dateIcon, localDate.strftime( "%A, %#d %B %Y at %#I:%M %p").replace( " at 12:00 PM", "")))
-        else:
-            newEntry.append( '## %s%s\n' % (dateIcon, localDate.strftime( "%A, %-d %B %Y at %-I:%M %p").replace( " at 12:00 PM", "")))  #untested
+        # if sys.platform == "win32":
+        #     newEntry.append(
+        #         '## %s%s\n' % (dateIcon, localDate.strftime("%A, %#d %B %Y at %#I:%M %p").replace(" at 12:00 PM", "")))
+        # else:
+        # newEntry.append('## %s%s\n' % (
+        #     dateIcon, localDate.strftime("%A, %-d %B %Y at %-I:%M %p").replace(" at 12:00 PM", "")))  # untested
 
         # Add body text if it exists (can have the odd blank entry), after some tidying up
         try:
-            newText = entry['text'].replace("\\", "")
-            newText = newText.replace( "\u2028", "\n")
-            newText = newText.replace( "\u1C6A", "\n\n")
+            if 'text' in entry:
+                newText = entry['text'].replace("\\", "")
+            else:
+                newText = DEFAULT_TEXT
+            newText = newText.replace("\u2028", "\n")
+            newText = newText.replace("\u1C6A", "\n\n")
 
             if 'photos' in entry:
                 # Correct photo links. First we need to rename them. The filename is the md5 code, not the identifier
                 # subsequently used in the text. Then we can amend the text to match. Will only to rename on first run
                 # through as then, they are all renamed.
                 # Assuming all jpeg extensions.
-
-                for p in entry['photos']:
-                    pfn = os.path.join( root, 'photos', '%s.jpeg' % p['md5'] )
-                    if os.path.isfile( pfn ):
-                        newfn = os.path.join( root, 'photos', '%s.jpeg' % p['identifier'] )
-                        print ( 'Renaming %s to %s' % (pfn, newfn ))
-                        os.rename( pfn, newfn )
+                photo_list = entry['photos']
+                for p in photo_list:
+                    photo_processor.add_entry_to_dict(p)
+                    rename_media(root, 'photos', p, p['type'])
 
                 # Now to replace the text to point to the file in obsidian
-                newText = re.sub(r"(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", r'![[\2.jpeg]]', newText)
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))",
+                                 photo_processor.replace_entry_id_with_info,
+                                 newText)
 
-            newEntry.append( newText )
-        except KeyError:
+            if 'audios' in entry:
+                audio_list = entry['audios']
+                for p in audio_list:
+                    audio_processor.add_entry_to_dict(p)
+                    rename_media(root, 'audios', p, "m4a")
+
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/audio\/)([A-F0-9]+)(\))",
+                                 audio_processor.replace_entry_id_with_info, newText)
+
+            if 'pdfAttachments' in entry:
+                pdf_list = entry['pdfAttachments']
+                for p in pdf_list:
+                    pdf_processor.add_entry_to_dict(p)
+                    rename_media(root, 'pdfs', p, p['type'])
+
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/pdfAttachment\/)([A-F0-9]+)(\))",
+                                 pdf_processor.replace_entry_id_with_info, newText)
+
+            if 'videos' in entry:
+                video_list = entry['videos']
+                for p in video_list:
+                    video_processor.add_entry_to_dict(p)
+                    rename_media(root, 'videos', p, p['type'])
+
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/video\/)([A-F0-9]+)(\))",
+                                 video_processor.replace_entry_id_with_info, newText)
+
+            newEntry.append(newText)
+
+        except Exception as e:
+            logging.error(f"Exception: {e}")
             pass
 
         ## Start Metadata section
 
-        newEntry.append( '\n\n---\n' )
+        newEntry.append('\n\n---\n')
 
+        # Add location
+        location = EntryProcessor.get_location_coordinate(entry)
         if not location == '':
-            if coordinates == []:
-                locationString =  location 
-            else:
-                locationString = '[' + location + '](geo:' + coordinates + ')'
-            newEntry.append( locationString ) 
+            newEntry.append(location)
 
         # Add GPS, not all entries have this
         # try:
@@ -118,45 +180,35 @@ with open(fn, encoding='utf-8') as json_file:
         # except KeyError:
         #     pass
 
-        tags = []
-        if 'tags' in entry:
-            tags = []
-            for t in entry['tags']:
-                tags.append( "%s%s" % (tagPrefix, t.replace(' ', '-').replace('---', '-') ) )
-            if entry['starred']:
-                tags.append( "%sstarred" % (tagPrefix) )
-        if len(tags) > 0:
-            newEntry.append( "- Tags: %s\n" % " ".join( tags ))
-
-
-
         # Save entries organised by year, year-month, year-month-day.md
-        yearDir = os.path.join( journalFolder, str(createDate.year) )
-        monthDir = os.path.join( yearDir, createDate.strftime( '%Y-%m'))
+        yearDir = os.path.join(journalFolder, str(createDate.year))
+        monthDir = os.path.join(yearDir, createDate.strftime('%Y-%m'))
 
         if not os.path.isdir(yearDir):
             os.mkdir(yearDir)
-        
+
         if not os.path.isdir(monthDir):
             os.mkdir(monthDir)
-        
+
+        title = EntryProcessor.get_title(entry)
+
+        # Filename format: "title localDate"
         # Target filename to save to. Will be modified if already exists
-        fnNew = os.path.join( monthDir, "%s.md" % localDate.strftime( '%Y-%m-%d'))
-        
+        fnNew = os.path.join(monthDir, "%s %s.md" % (title, localDate.strftime('%Y-%m-%d')))
+
         # Here is where we handle multiple entries on the same day. Each goes to it's own file
-        if os.path.isfile( fnNew):
+        if os.path.isfile(fnNew):
             # File exists, need to find the next in sequence and append alpha character marker
-            index = 97 #ASCII a
-            fnNew = os.path.join( monthDir, "%s%s.md" % (localDate.strftime( '%Y-%m-%d'), chr(index)))
+            index = 97  # ASCII a
+            fnNew = os.path.join(monthDir, "%s %s %s.md" % (title, localDate.strftime('%Y-%m-%d'), chr(index)))
             while os.path.isfile(fnNew):
                 index += 1
-                fnNew = os.path.join( monthDir, "%s%s.md" % (localDate.strftime( '%Y-%m-%d'), chr(index)))
-
+                fnNew = os.path.join(monthDir, "%s %s %s.md" % (title, localDate.strftime('%Y-%m-%d'), chr(index)))
 
         with open(fnNew, 'w', encoding='utf-8') as f:
-         for line in newEntry:
-            f.write(line)
+            for line in newEntry:
+                f.write(line)
 
         count += 1
 
-print ("Complete: %d entries processed." % count)
+print("Complete: %d entries processed." % count)
