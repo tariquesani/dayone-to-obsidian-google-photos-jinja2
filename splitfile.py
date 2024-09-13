fe97255eec5a4159e8f95bc735261a9fa713eb8b
Tarique Sani <tariquesani@gmail.com>
2024-09-13 20:21:51 +0530

    Some code cleanup and commenting

--- splitfile.py
+++ splitfile.py
@@ -19,11 +19,23 @@ from config.config import Config
 from processor.utils import setup_jinja2_env
 
 def rename_media(root, subpath, media_entry, filetype):
-    pfn = os.path.join(root, subpath, '%s.%s' % (media_entry['md5'], filetype))
-    if os.path.isfile(pfn):
-        newfn = os.path.join(root, subpath, '%s.%s' % (media_entry['identifier'], filetype))
-        print('Renaming %s to %s' % (pfn, newfn))
-        os.rename(pfn, newfn)
+    """
+    Renames a media file based on its identifier.
+
+    Args:
+        root (str): The root directory of the media file.
+        subpath (str): The subpath of the media file.
+        media_entry (dict): A dictionary containing the media entry information.
+        filetype (str): The file type of the media file.
+
+    Returns:
+        None
+    """
+    old_file_name = os.path.join(root, subpath, '%s.%s' % (media_entry['md5'], filetype))
+    if os.path.isfile(old_file_name):
+        new_file_name = os.path.join(root, subpath, '%s.%s' % (media_entry['identifier'], filetype))
+        print('Renaming %s to %s' % (old_file_name, new_file_name))
+        os.rename(old_file_name, new_file_name)
 
 
 DEFAULT_TEXT = Config.get("DEFAULT_TEXT", "")
@@ -34,46 +46,38 @@ EntryProcessor.initialize()
 
 # Set this as the location where your Journal.json file is located
 root = Config.get("ROOT")
-icons = True  # Set to true if you are using the Icons Plugin in Obsidian
 
 # name of folder where journal entries will end up
-rootJournalFolder = Config.get("JOURNAL_FOLDER")
-# fn = os.path.join(root, Config.get("JOURNAL_JSON"))
+root_journal_folder = Config.get("JOURNAL_FOLDER")
 
 # Clean out existing journal folder, otherwise each run creates new files
-if os.path.isdir(rootJournalFolder):
-    print("Deleting existing folder: %s" % rootJournalFolder)
-    shutil.rmtree(rootJournalFolder)
+if os.path.isdir(root_journal_folder):
+    print("Deleting existing folder: %s" % root_journal_folder)
+    shutil.rmtree(root_journal_folder)
 # Give time for folder deletion to complete. Only a problem if you have the folder open when trying to run the script
 time.sleep(2)
 
-if not os.path.isdir(rootJournalFolder):
-    print("Creating root journal folder: %s" % rootJournalFolder)
-    os.mkdir(rootJournalFolder)
+if not os.path.isdir(root_journal_folder):
+    print("Creating root journal folder: %s" % root_journal_folder)
+    os.mkdir(root_journal_folder)
 
-if icons:
-    print("Icons are on")
-    dateIcon = "`rir:Calendar2`"
-else:
-    print("Icons are off")
-    dateIcon = ""  # make 2nd level heading
-
-dayOneJournals = [x for x in os.listdir(root) if x.endswith(".json")]
+# Get all journal files
+dayone_journals = [x for x in os.listdir(root) if x.endswith(".json")]
 
 print("Begin processing entries")
 
-for journalIndex in dayOneJournals:
-    journalFolder = os.path.join(rootJournalFolder, journalIndex.replace(".json", ""))
+for journal_index in dayone_journals:
+    journal_folder = os.path.join(root_journal_folder, journal_index.replace(".json", ""))
     dash = "\n========================================"
-    print(dash + "\nBegin processing " + journalIndex + dash)
-    if os.path.isdir(journalFolder):
+    print(dash + "\nBegin processing " + journal_index + dash)
+    if os.path.isdir(journal_folder):
         print("journalFolder already exists...")
         import pdb
         pdb.set_trace()
-    os.mkdir(journalFolder)
+    os.mkdir(journal_folder)
 
     count = 0
-    with open(os.path.join(root, journalIndex), encoding='utf-8') as json_file:
+    with open(os.path.join(root, journal_index), encoding='utf-8') as json_file:
         data = json.load(json_file)
 
         photo_processor = PhotoEntryProcessor(os.path.join(root, "photos"))
@@ -84,15 +88,15 @@ for journalIndex in dayOneJournals:
         for entry in data['entries']:
             new_entry = []
 
-            createDate = dateutil.parser.isoparse(entry['creationDate'])
-            localDate = createDate.astimezone(
+            create_date = dateutil.parser.isoparse(entry['creationDate'])
+            local_date = create_date.astimezone(
                 pytz.timezone(entry['timeZone']))  # It's natural to use our local date/time as reference point, not UTC
 
             env = setup_jinja2_env(template_path=os.path.join(os.path.dirname(__file__), './templates'))
             template = env.get_template("frontmatter_template.md")
 
             # Format the date and time with the weekday
-            formatted_datetime = createDate.isoformat() + 'Z'
+            formatted_datetime = create_date.isoformat() + 'Z'
 
             date_created = formatted_datetime
             coordinates = ''
@@ -107,14 +111,14 @@ for journalIndex in dayOneJournals:
             template = env.get_template("content_template.md")
 
             if sys.platform == "win32":
-                 heading = '\n## %s%s\n' % (dateIcon, localDate.strftime("%A, %#d %B %Y at %#I:%M %p").replace(" at 12:00 PM", ""))
+                 heading = ' %s\n' % (local_date.strftime("%A, %#d %B %Y at %#I:%M %p").replace(" at 12:00 PM", ""))
             else:
-                 heading = ('## %s%s\n' % (dateIcon, localDate.strftime("%A, %-d %B %Y at %-I:%M %p").replace(" at 12:00 PM", "")))  # untested
+                 heading = (' %s\n' % (local_date.strftime("%A, %-d %B %Y at %-I:%M %p").replace(" at 12:00 PM", "")))  # untested
 
             # Add body text if it exists (can have the odd blank entry), after some tidying up
             title = EntryProcessor.get_title(entry)
 
-            print("Processing entry: "+ localDate.strftime('%Y-%m-%d-%A'))
+            print("Processing entry: "+ local_date.strftime('%Y-%m-%d-%A'))
 
             try:
                 if 'text' in entry:
@@ -183,7 +187,7 @@ for journalIndex in dayOneJournals:
             # Get location
             location = EntryProcessor.get_location_coordinate(entry)
 
-            content = template.render(title=title,
+            content = template.render(heading=heading,
                                       content=new_text,
                                       location=location)
 
@@ -198,8 +202,8 @@ for journalIndex in dayOneJournals:
             #     pass
 
             # Save entries organised by year, year-month, year-month-day-weekday.md
-            yearDir = os.path.join(journalFolder, str(createDate.year))
-            monthDir = os.path.join(yearDir, createDate.strftime('%m-%B'))
+            yearDir = os.path.join(journal_folder, str(create_date.year))
+            monthDir = os.path.join(yearDir, create_date.strftime('%m-%B'))
             
             if not os.path.isdir(yearDir):
                  os.mkdir(yearDir)
@@ -210,16 +214,16 @@ for journalIndex in dayOneJournals:
             title = EntryProcessor.get_title(entry)
 
             # Filename format: "localDate"
-            fnNew = os.path.join(monthDir, "%s.md" % (localDate.strftime('%Y-%m-%d-%A')))
+            fnNew = os.path.join(monthDir, "%s.md" % (local_date.strftime('%Y-%m-%d-%A')))
 
             # Here is where we handle multiple entries on the same day. Each goes to it's own file
             if os.path.isfile(fnNew):
                 # File exists, need to find the next in sequence and append alpha character marker
                 index = 97  # ASCII a
-                fnNew = os.path.join(journalFolder, "%s %s.md" % (title, chr(index)))
+                fnNew = os.path.join(journal_folder, "%s %s.md" % (title, chr(index)))
                 while os.path.isfile(fnNew):
                     index += 1
-                    fnNew = os.path.join(journalFolder, "%s %s.md" % (title, chr(index)))
+                    fnNew = os.path.join(journal_folder, "%s %s.md" % (title, chr(index)))
 
             with open(fnNew, 'w', encoding='utf-8') as f:
                 for line in new_entry:
@@ -227,21 +231,21 @@ for journalIndex in dayOneJournals:
 
             # Set created date and last modified date to entry's date
             if (os.path.exists(fnNew)):
-                date_epoch = createDate.timestamp()
+                date_epoch = create_date.timestamp()
                 os.utime(fnNew, (date_epoch, date_epoch))
             else:
                 print("Write to file failed.")
 
             count += 1
-    print("Journal %s complete. %d entries processed." % (journalIndex, count))
+    print("Journal %s complete. %d entries processed." % (journal_index, count))
 
 print("\n")
 for media_type in [os.path.join("videos", THUMBNAILS_FOLDER), "photos", "pdfs", "audios"]:
     media_folder_path = os.path.join(root, media_type)
     if os.path.exists(media_folder_path):
-        print("Copying %s to %s" % (media_type, rootJournalFolder))
-        shutil.copytree(media_folder_path, os.path.join(rootJournalFolder, media_type))
+        print("Copying %s to %s" % (media_type, root_journal_folder))
+        shutil.copytree(media_folder_path, os.path.join(root_journal_folder, media_type))
     else:
         print("%s folder does not exist" % media_type.capitalize())
 
-print("\nComplete. %d journals processed." % len(dayOneJournals))
+print("\nComplete. %d journals processed." % len(dayone_journals))
